ec54caff51adfe34731dbcf2133f7795d4a48fd8
Niheeth Thummala <88976098+niheeth03@users.noreply.github.com>
2022-03-07 09:14:00 +0530

    Bubble Sort

--- fd.asm
+++ fd.asm
@@ -1,32 +1,32 @@
-.data
-arr: .word 80, 50, 70, 30, 20, 45, 60, 10, 0, 90, 55, 25
+  .data
+arr: .word 999, 500, 30, 190, 570, 20, 185, 670, 750, 870, 55
   space: .asciiz " "
   .text
   .globl main
 
 main:
   lui s0, 0x1001                   
-  li t0, 0                        
+  li t0, 0                         
   li t1, 0                         
-  li s1, 11                       
+  li s1, 11                        
   li s2, 11                        
   add t2, zero, s0               
-  add t3, zero, s0            
+  add t3, zero, s0               
 
   addi s1, s1, -1
 
-oloop:
-  li  t1, 0                       
-  addi s2, s2, -1                 
-  add t3, zero, s0               
+outer_loop:
+  li  t1, 0                        
+  addi s2, s2, -1                
+  add t3, zero, s0              
 
-  iloop:
-    lw s3, 0(t3)                 
-    addi t3, t3, 4               
-    lw s4, 0(t3)                 
+  inner_loop:
+    lw s3, 0(t3)                  
+    addi t3, t3, 4                
+    lw s4, 0(t3)                  
     addi t1, t1, 1                
 
-    slt t4, s3, s4              
+    slt t4, s3, s4               
     bne t4, zero, cond
     swap:
       sw s3, 0(t3)
@@ -34,14 +34,13 @@ oloop:
       lw s4, 0(t3)
 
     cond:
-      bne t1, s2, iloop      
+      bne t1, s2, inner_loop      
 
     addi t0, t0, 1                  
-  bne t0, s1, oloop           
-
+  bne t0, s1, outer_loop           
   li t0, 0
   addi s1, s1, 1
-ploop:
+print_loop:
   li a7, 1
   lw a0, 0(t2)
   ecall
@@ -50,9 +49,8 @@ ploop:
   ecall
 
   addi t2, t2, 4                  
-  addi t0, t0, 1                  
-  bne t0, s1, ploop          
-
+  addi t0, t0, 1                 
+  bne t0, s1, print_loop         
 exit:
   li a7, 10
   ecall
